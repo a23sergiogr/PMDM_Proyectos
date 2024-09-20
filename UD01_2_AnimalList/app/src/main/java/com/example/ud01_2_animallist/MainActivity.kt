a85688dd199d0985e8a405b1e5039721795b2f7e
dam2 - Gómez Rial <A23SergioGR@sanclemente.local>
2024-09-20 14:32:20 +0200

    finnish ex2 start ex3

--- UD01_2_AnimalList/app/src/main/java/com/example/ud01_2_animallist/MainActivity.kt
+++ UD01_2_AnimalList/app/src/main/java/com/example/ud01_2_animallist/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.ud01_2_animallist
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,13 +16,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnSend = findViewById<Button>(R.id.buttonSend)
-        //btnSend.setOnClickListener()
-
+        btnSend.setOnClickListener {
+            val spinnerAnimalType = findViewById<Spinner>(R.id.spinnerAnimalList)
+            val textKindAnimal = findViewById<TextView>(R.id.textKindsAnimals)
+            textKindAnimal.text = "Has seleccionado el ${spinnerAnimalType.selectedItemId}\n ${getAnimalKind(spinnerAnimalType.selectedItemId)}"
+            /*
+            """Has seleccionado el ${spinnerAnimalType.selectedItemId}
+            ${this.getAnimalKind(id = spinnerAnimalType.selectedItemId)}"""
+            .also { textKindAnimal.text = it }
+            */
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun getAnimalKind(id:Long) = when (id){
+        0L -> listOf("Chiguagua", "Pastor Alemán")
+        1L -> listOf("Persa", "Egipcio")
+        2L -> listOf("Mallard", "White call")
+        else -> listOf()
     }
 }
